52a97cce2425be097877780b63c46df51f596cbc
SouravSinghRana <41117593+SouravSinghRana@users.noreply.github.com>
2021-03-15 20:13:27 +0530

    implemted responsive grid

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyA3MYV1ppqxGWXAcIBaPnpAhU3X8A62usU';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
     baseURL : 'https://www.googleapis.com/youtube/v3',
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,8 +4,10 @@ import VideoItem from './VideoItem';
 const VideoList = (props) => {
 
     const renderedList = props.videos.map((video) => {
-        console.log(video)
-        return <VideoItem key={video.snippet.title} onVideoSelect={props.onVideoSelect} video={video}/>
+        return <VideoItem 
+                key={video.snippet.title}
+                onVideoSelect={props.onVideoSelect} 
+                video={video}/>
     });
 
     return (
